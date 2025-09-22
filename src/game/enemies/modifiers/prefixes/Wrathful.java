52f69cd27132beba495b0827ed113f1b09d7874d
Brandon Coburn <bcoburn1@frontiernet.net>
2025-09-22 08:12:19 -0400

    update Wrathful prefix to test git with

--- src/game/enemies/modifiers/prefixes/Wrathful.java
+++ src/game/enemies/modifiers/prefixes/Wrathful.java
@@ -8,7 +8,7 @@ public class Wrathful implements Prefix {
     @Override
     public void apply(Enemy enemy) {
         enemy.getAttributes().increaseStrength(15);
-        enemy.getAttributes().increaseAgility(5);
+        enemy.getAttributes().increaseAgility(10);
         enemy.getAttributes().decreaseDefense(10);
     }
 
