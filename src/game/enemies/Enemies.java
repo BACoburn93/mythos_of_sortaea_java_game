e5ea0203254c092d5010f049db59b6a7ab497299
Brandon Coburn <bcoburn1@frontiernet.net>
2025-09-22 08:02:04 -0400

    remove unused enemies file

---
+++ src/game/enemies/Enemies.java
@@ -0,0 +1,22 @@
+package enemies;
+
+// import java.util.Arrays;
+
+public class Enemies {
+    Enemy[] enemies;
+
+    Enemies(Enemy[] enemies) {
+        this.enemies = enemies;
+    }
+
+    // private static Enemy[] removeDefeatedEnemy(Enemy[] actors, int index) {
+    //     Enemy[] newEnemies = new Enemy[actors.length - 1];
+    //     for (int i = 0, j = 0; i < actors.length; i++) {
+    //         if (i != index) {
+    //             newEnemies[j++] = actors[i];
+    //         }
+    //     }
+    //     System.out.println(Arrays.toString(actors) + " " + Arrays.toString(newEnemies));
+    //     return newEnemies;
+    // }
+}
