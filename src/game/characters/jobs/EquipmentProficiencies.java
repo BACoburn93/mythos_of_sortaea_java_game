271c7100a373d68036d72e9b2b7ebe667011b9f6
Brandon Coburn <bcoburn1@frontiernet.net>
2025-10-15 04:57:33 -0400

    fix the EquipmentProficiencies sets to match new sword and bow classes

--- src/game/characters/jobs/EquipmentProficiencies.java
+++ src/game/characters/jobs/EquipmentProficiencies.java
@@ -6,7 +6,6 @@ import items.equipment.item_types.*;
 public class EquipmentProficiencies {
     public static final Set<ItemType> mageAllowed = Set.of(
         WeaponTypes.DAGGER,
-        WeaponTypes.SHORTSWORD,
         WeaponTypes.STAFF, 
         WeaponTypes.WAND, 
         WeaponTypes.TOME, 
@@ -16,7 +15,7 @@ public class EquipmentProficiencies {
 
     public static final Set<ItemType> warriorAllowed = Set.of(
         WeaponTypes.BATTLEAXE,
-        WeaponTypes.BROADSWORD,
+        WeaponTypes.BOW,
         WeaponTypes.CLAYMORE,
         WeaponTypes.CROSSBOW,
         WeaponTypes.DAGGER,
@@ -25,15 +24,12 @@ public class EquipmentProficiencies {
         WeaponTypes.HALBERD,
         WeaponTypes.JAVELIN,
         WeaponTypes.KATANA,
-        WeaponTypes.BOW,
-        WeaponTypes.SWORD,
         WeaponTypes.MACE,
         WeaponTypes.RAPIER,
         WeaponTypes.SCIMITAR,
         WeaponTypes.SCYTHE,
-        WeaponTypes.SHORTBOW,
-        WeaponTypes.SHORTSWORD,
         WeaponTypes.SPEAR,
+        WeaponTypes.SWORD,
         WeaponTypes.TRIDENT,
         WeaponTypes.WARHAMMER,
         WeaponTypes.WHIP,
@@ -47,17 +43,15 @@ public class EquipmentProficiencies {
     );
 
     public static final Set<ItemType> rogueAllowed = Set.of(
+        WeaponTypes.BOW,
         WeaponTypes.DAGGER,
         WeaponTypes.FISTICUFFS,
         WeaponTypes.FLAIL,
         WeaponTypes.KATANA,
-        WeaponTypes.BOW,
-        WeaponTypes.SWORD,
         WeaponTypes.RAPIER,
         WeaponTypes.SCIMITAR,
-        WeaponTypes.SHORTBOW,
-        WeaponTypes.SHORTSWORD,
         WeaponTypes.SPEAR,
+        WeaponTypes.SWORD,
         WeaponTypes.WHIP,
         ArmorTypes.MEDIUM,
         ArmorTypes.LIGHT,
@@ -67,17 +61,15 @@ public class EquipmentProficiencies {
 
     public static final Set<ItemType> rangerAllowed = Set.of(
         WeaponTypes.BOW,
-        WeaponTypes.SHORTBOW,
         WeaponTypes.SPEAR,
         WeaponTypes.JAVELIN,
         WeaponTypes.CROSSBOW,
         WeaponTypes.DAGGER,
         WeaponTypes.FISTICUFFS,
         WeaponTypes.KATANA,
-        WeaponTypes.SWORD,
         WeaponTypes.RAPIER,
         WeaponTypes.SCIMITAR,
-        WeaponTypes.SHORTSWORD,
+        WeaponTypes.SWORD,
         WeaponTypes.WHIP,
         ArmorTypes.MEDIUM,
         ArmorTypes.LIGHT,
